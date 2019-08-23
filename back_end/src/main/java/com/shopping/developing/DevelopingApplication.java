4e527e587c339abee005d4b7f62ed7f0c9171650
Yurii <89320dota@gmail.com>
2019-08-23 17:54:33 +0300

    init commit

---
+++ back_end/src/main/java/com/shopping/developing/DevelopingApplication.java
@@ -0,0 +1,13 @@
+package com.shopping.developing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopingApplication.class, args);
+    }
+
+}
